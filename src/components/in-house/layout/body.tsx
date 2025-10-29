8cc08a82638a7230ce29446ad58805ba905837c5
Martin Etchebarne <martinetchebarne829@gmail.com>
2025-10-29 12:28:27 -0300

    feat: add ScrambleIn component for animated text display; update Body and Home components to utilize new functionality; add new registry for Fancy components

--- src/components/in-house/layout/body.tsx
+++ src/components/in-house/layout/body.tsx
@@ -1,3 +1,21 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import ScrambleIn from "@/components/fancy/text/scramble-in";
+
 export default function Body() {
-  return <div></div>;
+  return (
+    <div className="w-dvw h-dvh flex flex-col gap-6 items-center justify-center">
+      <Avatar className="size-36 sm:size-42 select-none animate-in slide-in-from-top fade-in-0 duration-1000">
+        <AvatarImage draggable={false} src="https://github.com/martinetbn.png" />
+        <AvatarFallback>M</AvatarFallback>
+      </Avatar>
+      <div className="flex flex-col gap-2 items-center select-none">
+        <span className="text-4xl font-semibold text-center">
+          <ScrambleIn text="Martin Etchebarne" />
+        </span>
+        <div className="text-center text-gray-700 dark:text-gray-400">
+          <ScrambleIn text="software developer" />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/in-house/views/home.tsx
+++ src/components/in-house/views/home.tsx
@@ -1,8 +1,10 @@
+import Body from "../layout/body";
 import CustomDock from "../layout/dock";
 
 export default function Home() {
   return (
     <main>
+      <Body />
       <CustomDock />
     </main>
   );
